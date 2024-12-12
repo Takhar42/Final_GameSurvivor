c7651611c75d93915d7a7232fe6f976e33eb53b1
hamzaahmed1234 <126507784+hamzaahmed1234@users.noreply.github.com>
2024-12-11 20:11:14 -0800

    more movement mechanics

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,21 +7,30 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jump_audio: AudioStreamPlayer2D
 
 func _ready():
-	jump_audio = $JumpAudio # Initialize jump_audio properly here
+	jump_audio = $JumpAudio  # Initialize jump_audio properly here
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	
+	velocity.y += gravity * delta
+	if is_on_floor():
+		$CollisionShape2D.disabled = false
+		if Input.is_action_pressed("ui_accept"):
+			$AnimatedSprite2D.play("run")
+			# $JumpSound.play()
+			
+		elif Input.is_action_pressed("ui_up"):
+			velocity.y = JUMP_VELOCITY
+			$AnimatedSprite2D.play("jump")
+			
+		elif Input.is_action_pressed("ui_down"):
+			$AnimatedSprite2D.play("slide")
+			$CollisionShape2D.disabled = true
+			
+		elif Input.is_action_pressed("attack"):
+			$AnimatedSprite2D.play("attack")
+			
+		else:
+			$AnimatedSprite2D.play("run")
 
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		jump_audio.play()
-		
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * MOVE_SPEED
-	else:
-		velocity.x = 0
-	
+
 	move_and_slide()
-	
